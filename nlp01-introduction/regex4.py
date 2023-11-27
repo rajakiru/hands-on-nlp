52ebb27ee24bf40dd7d6c0d7cc1f9f8d2c1a1350
Kiruthika <rajakiru02@gmail.com>
2023-11-27 16:07:00 +0900

    files updates

--- nlp01-introduction/regex4.py
+++ nlp01-introduction/regex4.py
@@ -1,6 +1,3 @@
-# Introduction to Python Regular Expressions
-
-# Importing the libraries
 import re
 
 sentence1 = "Welcome to the year 2018"
@@ -8,13 +5,8 @@ sentence2 = "Just ~%* ++++--- arrived at @Jack's place. #fun"
 sentence3 = "I                  love                u"
 
 sentence1_modified = re.sub(r'\d','',sentence1)
-
 sentence2_modified = re.sub(r'[@#\.\']','',sentence2)
-
 sentence2_modified = re.sub(r'\W',' ',sentence2)
-
 sentence2_modified = re.sub(r'\s+',' ',sentence2_modified)                               
-
 sentence2_modified = re.sub(r"\s+[a-zA-Z]\s+",' ',sentence2_modified)
-
 sentence3_modified = re.sub(r'\s+',' ',sentence3)                           
